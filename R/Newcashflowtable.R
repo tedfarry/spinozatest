57487c8af02de6846a62586c775a84a4e819b5fb
Ted Farry <houses@conatusre.com>
2019-01-27 23:41:21 -0800

    Update Newcashflowtable.R

--- R/Newcashflowtable.R
+++ R/Newcashflowtable.R
@@ -1,6 +1,6 @@
 
 
-CFReturnTable <- function(RentAmt, RentAppRate, ValAppRate, TurnTime, ATenStay, DLQ, PMFee, LC, InsRate, TurnCost, CapexMaint, taxrate, ppval, hoamonthly, sqft, HoldPeriod, RehabVal, CCRate, SaleCC, DiscountToPurchase, LTV, IntRate, Amort) {
+CFReturnTable <- function(RentAmt, RentAppRate, ValAppRate, TurnTime, ATenStay, DLQ, PMFee, LC, InsRate, TurnCost, CapexMaint, taxrate, ppval, hoamonthly, sqft, HoldPeriod, RehabVal, CCRate, SaleCC, DiscountToPurchase, LTV, IntRate, Amort, Points) {
   library(FinCal)
   library(flextable)
 
@@ -24,7 +24,7 @@ CFReturnTable <- function(RentAmt, RentAppRate, ValAppRate, TurnTime, ATenStay, 
   ToteCF <- PMFCF + LCCF + InsCF + MaCeCF + TaxCF + HoaCF
   IncCF <- GICF - ToteCF
   LoanPMT <- c(0,rep(pmt(IntRate/1200, Amort*12, -ppval*(LTV/100),0)*12,HoldPeriod))
-  LoanAMT <- c(ppval*(LTV/100),fv(IntRate/1200, (1:HoldPeriod)*12, -ppval*(LTV/100), pmt(IntRate/1200, Amort*12, -ppval*(LTV/100),0)))
+  LoanAMT <- c((ppval*(LTV/100))*(1+(Points/100)),fv(IntRate/1200, (1:HoldPeriod)*12, -ppval*(LTV/100), pmt(IntRate/1200, Amort*12, -ppval*(LTV/100),0)))
   LevCF <- c(-TotInv + LoanAMT[1], IncCF[2:HoldPeriod] - LoanPMT[2:HoldPeriod], (PropVal[HoldPeriod+1]*(1-(SaleCC/100)))+IncCF[(HoldPeriod+1)] - LoanPMT[(HoldPeriod+1)] - LoanAMT[(HoldPeriod+1)])
 
   rnames <- c("Rent" , "Vacancy" , "Delinquency" , "Gross Income" , "Property Management Fee" , "Leasing Commission" , "Insurance" , "Maintenance/CapEx" , "Property Tax" , "HOA" , "Total Expenses" , "Income" , "Property Value" , "Outstanding Loan Balance" , "Loan Payment" , "Levered Cash Flow")
