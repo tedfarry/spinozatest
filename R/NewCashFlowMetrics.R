0016ab0da8bde0ce8665d5014dc3fd00fe0e776e
Ted Farry <houses@conatusre.com>
2020-05-12 21:12:44 -0700

    Update NewCashFlowMetrics.R

--- R/NewCashFlowMetrics.R
+++ R/NewCashFlowMetrics.R
@@ -1,5 +1,7 @@
 CFReturnMetrics <- function(RentAmt, RentAppRate, ValAppRate, TurnTime, ATenStay, DLQ, PMFee, LC, InsRate, TurnCost, CapexMaint, taxrate, ppval, hoamonthly, sqft, HoldPeriod, RehabVal, CCRate, SaleCC, DiscountToPurchase, LTV, IntRate, Amort, Points) {
  
+  library("devtools")
+  install_github("felixfan/FinCal") # from GitHub
   library(FinCal)
   library(rjson)
   
